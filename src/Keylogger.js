b11a6dd9fb6693c941df423a9b30f93f2462e1ef
lewibs <benjaminsl2000@gmail.com>
2022-07-09 01:33:29 -0400

    finished testing reduced public methods

--- src/Keylogger.js
+++ src/Keylogger.js
@@ -3,153 +3,145 @@ const LEFTMOUSE = "leftmouse";
 const MIDDLEMOUSE = "middlemouse";
 const RIGHTMOUSE = "rightmouse";
 
-class Tracker {
+class Keylogger {
     
     constructor() {
-        this.isTracker  = true;
+        this.isKeylogger  = true;
         this.target = window;
-        this.pressedKeys = {};
+        this.pressed = {};
         this.history = [];
         this.events = [];
 
-        //make custom events - nameend and namestart
-        this.splitEvent('wheel');
-        this.splitEvent('mousemove');
-        
-        //key
-        this.addEvent('keyup', (e)=>{this.unpressed(e)});
-        this.addEvent('keydown', (e)=>{this.pressed(e)});
-        
-        //mouse
-        this.addEvent('mousedown', (e)=>{this.pressed(e)});
-        this.addEvent('mouseup', (e)=>{this.unpressed(e)});
-
-        //wheel
-        this.addEvent('wheelstart', (e)=>{this.pressed(e)});
-        this.addEvent('wheelend', (e)=>{this.unpressed(e)});
-        
-        //mousemove
-        this.addEvent('mousemovestart', (e)=>{this.pressed(e)});
-        this.addEvent('mousemoveend', (e)=>{this.unpressed(e)});
+        this.start();
     }
-    
-    addEvent(trigger, event) {
 
-        const dispatch = (e) => {
-            e = this.tunnel(e);
-            event(e);
+    start() {
+        const pressed = (e) => {
+            this.pressed[e.key] = true;
+        }
+        
+        const unpressed = (e) => {
+            delete this.pressed[e.key];
         }
 
-        this.events.push({
-            trigger: trigger,
-            dispatch: dispatch,
-        });
+        const addEvent = (trigger, event) => {
 
-        this.target.addEventListener(trigger, dispatch);
-    }
-
-    splitEvent(name) {
-        let startName = name + 'start';
-        let endName = name + 'end';
+            const tunnel = (event) => {
+                const addToHistory = (event) => {
         
-        let timer;
+                    this.history.push({
+                        pressed: Object.assign({}, this.pressed),
+                        event: event,
+                        time: new Date().getTime(),
+                    });
         
-        let formatCustom = (name, e) => {
-            return new CustomEvent(name, {detail: e});
-        };
+                    return event;
+                };
         
-        let eventEnd = (e)=>{
-            this.target.dispatchEvent(formatCustom(endName, e));
-            timer = undefined;
-        };
+                const format = (event) => {
+                    if (event instanceof CustomEvent) {
+                        let key = event.detail.type;
+                        event = event.detail;
+                        event.key = key;
+                    } else {
+                        switch (event.which) {
+                            case 0:
+                                event.key = WHEEL;   
+                                break;
+                            case 1:
+                                event.key = LEFTMOUSE;
+                                break;
+                            case 2:
+                                event.key = MIDDLEMOUSE;
+                                break;
+                            case 3:
+                                event.key = RIGHTMOUSE;
+                                break;
+                        }   
+                    }
+                    
+                    return event;
+                };
         
-        let eventStart = (e)=>{
-            if (timer === undefined) {
-                this.target.dispatchEvent(formatCustom(startName, e));
+                event = format(event);
+                event = addToHistory(event);
+                return event;
             }
-            
-            clearTimeout(timer);
-            timer = setTimeout(()=>{eventEnd(e)}, 100);
-        };
 
-        this.addEvent(name, eventStart);
-    }
-
-    tunnel(event) {
-        const addToHistory = (event) => {
-
-            this.history.push({
-                pressedKeys: Object.assign({}, this.pressedKeys),
-                event: event,
-                time: new Date().getTime(),
-            });
-
-            return event;
-        };
-
-        const format = (event) => {
-            if (event instanceof CustomEvent) {
-                let key = event.detail.type;
-                event = event.detail;
-                event.key = key;
-            } else {
-                switch (event.which) {
-                    case 0:
-                        event.key = WHEEL;   
-                        break;
-                    case 1:
-                        event.key = LEFTMOUSE;
-                        break;
-                    case 2:
-                        event.key = MIDDLEMOUSE;
-                        break;
-                    case 3:
-                        event.key = RIGHTMOUSE;
-                        break;
-                }   
+            const dispatch = (e) => {
+                e = tunnel(e);
+                event(e);
             }
-            
-            return event;
-        };
-
-        event = format(event);
-        event = addToHistory(event);
-        return event;
-    }
-
-    pressed(e) {
-        this.pressedKeys[e.key] = true;
-    }
     
-    unpressed(e) {
-        delete this.pressedKeys[e.key];
+            this.events.push({
+                trigger: trigger,
+                dispatch: dispatch,
+            });
+    
+            this.target.addEventListener(trigger, dispatch);
+        }
+
+        const splitEvent = (name) => {
+            let startName = name + 'start';
+            let endName = name + 'end';
+            let timer;
+            
+            let formatCustom = (name, e) => {
+                return new CustomEvent(name, {detail: e});
+            };
+            
+            let eventEnd = (e)=>{
+                this.target.dispatchEvent(formatCustom(endName, e));
+                timer = undefined;
+            };
+            
+            let eventStart = (e)=>{
+                if (timer === undefined) {
+                    this.target.dispatchEvent(formatCustom(startName, e));
+                }
+                
+                clearTimeout(timer);
+                timer = setTimeout(()=>{eventEnd(e)}, 100);
+            };
+    
+            addEvent(name, eventStart);
+        }
+
+        //make custom events - nameend and namestart
+        splitEvent('wheel');
+        splitEvent('mousemove');
+        
+        //key
+        addEvent('keyup', (e)=>{unpressed(e)});
+        addEvent('keydown', (e)=>{pressed(e)});
+        
+        //mouse
+        addEvent('mousedown', (e)=>{pressed(e)});
+        addEvent('mouseup', (e)=>{unpressed(e)});
+
+        //wheel
+        addEvent('wheelstart', (e)=>{pressed(e)});
+        addEvent('wheelend', (e)=>{unpressed(e)});
+        
+        //mousemove
+        addEvent('mousemovestart', (e)=>{pressed(e)});
+        addEvent('mousemoveend', (e)=>{unpressed(e)});
     }
 
-    kill() {
+    stop() {
         this.events.forEach((v)=>{
             this.target.removeEventListener(v.trigger, v.dispatch);
         });
     }
-}
-
-class Keylogger {
-    constructor() {
-        this._logger = new Tracker();
-        
-        this.isKeyLogger = true;
-        this.history = this._logger.history;
-        this.pressed = this._logger.pressedKeys;
-    }
 
     kill() {
-        this._logger.kill();
+        this.stop();
+        Object.keys(this).forEach((key)=>{
+            this[key] = undefined;
+        });
     }
 }
 
-const instance = new Keylogger(window);
-Object.freeze(instance);
-
 module.exports = {
-    globalinstance: instance,
     Keylogger: Keylogger,
 }
--- test/src/App.js
+++ test/src/App.js
@@ -1,20 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import {globalinstance} from 'lewibs-keylogger';
+import {Keylogger} from 'lewibs-keylogger';
 
 function App() {
+  const [logger, setLogger] = React.useState(new Keylogger());
   const [toggle, setToggle] = React.useState(false);
-  const lastHistory = globalinstance.history[globalinstance.history.length - 1] || {};
+
+  const lastHistory = logger.history[logger.history.length - 1] || {};
 
   React.useEffect(()=>{
     window.addEventListener("keydown", ()=>{
-      console.log("window: ", globalinstance);
       setToggle(p=>!p);
     });
 
     window.addEventListener("keyup", ()=>{
-      console.log("window: ", globalinstance);
       setToggle(p=>!p);
     });
   }, []);
@@ -23,27 +23,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Try pressing some keys:
-        </p>
         <div>
-          current keys
-          <p>
-            {JSON.stringify(globalinstance.pressed)}
-          </p>
+          can also be stopped, started, and killed
         </div>
-        <br/>
         <div>
-          last key press:
-          <p>
-            pressed keys: {JSON.stringify(lastHistory.pressedKeys)}
-          </p>
-          <p>
-            time: {JSON.stringify(lastHistory.time)}
-          </p>
-          <p>
-            event: {JSON.stringify(lastHistory.event)}
-          </p>
+          <h4>current keys</h4>
+          {JSON.stringify(logger.pressed)}
+        </div>
+        <div>
+          <h4>last history input</h4>
+          pressed keys: {JSON.stringify(lastHistory.pressed)}<br/>
+          time: {JSON.stringify(lastHistory.time)}<br/>
+          event: {JSON.stringify(lastHistory.event)}<br/>
         </div>
       </header>
     </div>
